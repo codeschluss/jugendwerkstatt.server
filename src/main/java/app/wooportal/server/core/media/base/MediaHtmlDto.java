3112deb88c96edfea03d9a821a04c159179437f7
vetemi <valmir@codeschluss.de>
2022-05-08 20:15:22 +0200

    fixed html to pdf export

--- src/main/java/app/wooportal/server/core/media/base/MediaHtmlDto.java
+++ src/main/java/app/wooportal/server/core/media/base/MediaHtmlDto.java
@@ -15,4 +15,16 @@ public class MediaHtmlDto {
   private String name;
   
   private String html;
+  
+  public String getHtml() {
+    if (!html.contains("<body>")) {
+      html = String.format("<body>%s</body>", html);
+    }
+    
+    if (!html.contains("<html>")) {
+      html = String.format("<html>%s</html>", html);
+    }
+    
+    return html;
+  }
 }
