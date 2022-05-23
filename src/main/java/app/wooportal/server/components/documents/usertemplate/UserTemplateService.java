58b21eaa50d23d0f50e9f226b42f399e653a8766
vetemi <valmir@codeschluss.de>
2022-05-23 09:33:49 +0200

    removed unique constraint in name

--- src/main/java/app/wooportal/server/components/documents/usertemplate/UserTemplateService.java
+++ src/main/java/app/wooportal/server/components/documents/usertemplate/UserTemplateService.java
@@ -1,6 +1,5 @@
 package app.wooportal.server.components.documents.usertemplate;
 
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 import app.wooportal.server.core.base.DataService;
 import app.wooportal.server.core.repository.DataRepository;
@@ -14,14 +13,5 @@ public class UserTemplateService
       UserTemplatePredicateBuilder predicate) {
     super(repo, predicate);
   }
-
-  @Override
-  public Optional<UserTemplateEntity> getExisting(UserTemplateEntity entity) {
-    return entity.getName() == null || entity.getName().isEmpty() ? Optional.empty()
-        : getByName(entity.getName());
-  }
-
-  public Optional<UserTemplateEntity> getByName(String name) {
-    return repo.findOne(predicate.withName(name));
-  }
+  
 }
