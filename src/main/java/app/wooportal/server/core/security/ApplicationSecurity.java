b6b2ccaa83f8a919791de054fef06b4800e90377
vetemi <valmir@codeschluss.de>
2022-05-25 12:58:50 +0200

    disable CORS for production

--- src/main/java/app/wooportal/server/core/security/ApplicationSecurity.java
+++ src/main/java/app/wooportal/server/core/security/ApplicationSecurity.java
@@ -2,6 +2,7 @@ package app.wooportal.server.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -54,7 +55,7 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
   }
 
   @Bean
-//  @Profile(value = { "development", "local" })
+  @Profile(value = { "development", "local" })
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
 
