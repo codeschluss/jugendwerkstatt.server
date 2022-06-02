acdebf85c732193524884e275e0c76524e0a8ef1
DOOMED2WIN <timon@codeschluss.de>
2022-06-02 10:35:09 +0200

    chat

--- src/main/java/app/wooportal/server/components/message/chat/ChatRepository.java
+++
@@ -1,9 +0,0 @@
-package app.wooportal.server.components.message.chat;
-
-import app.wooportal.server.core.repository.DataRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface ChatRepository extends DataRepository<ChatEntity> {
-
-}
