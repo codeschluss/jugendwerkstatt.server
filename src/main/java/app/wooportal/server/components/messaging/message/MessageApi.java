4d97db1c1ccf4ca15d4858e40f447668bff901d0
vetemi <valmir@codeschluss.de>
2022-06-02 14:55:16 +0200

    message subscriptions (wip)

--- src/main/java/app/wooportal/server/components/messaging/message/MessageApi.java
+++ src/main/java/app/wooportal/server/components/messaging/message/MessageApi.java
@@ -2,31 +2,22 @@ package app.wooportal.server.components.messaging.message;
 
 import java.util.List;
 import java.util.Optional;
-import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Component;
 import app.wooportal.server.core.base.CrudApi;
 import app.wooportal.server.core.base.dto.listing.FilterSortPaginate;
 import app.wooportal.server.core.base.dto.listing.PageableList;
-import app.wooportal.server.core.error.exception.InvalidTokenException;
-import app.wooportal.server.core.security.services.AuthorizationService;
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
-import io.leangen.graphql.annotations.GraphQLSubscription;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
 @GraphQLApi
 @Component
 public class MessageApi extends CrudApi<MessageEntity, MessageService> {
-  
-  private final AuthorizationService authService;
 
   public MessageApi(
-      MessageService MessageService,
-      AuthorizationService authService) {
+      MessageService MessageService) {
     super(MessageService);
-    
-    this.authService = authService;
   }
 
   @Override
@@ -67,17 +58,7 @@ public class MessageApi extends CrudApi<MessageEntity, MessageService> {
   public Boolean deleteOne(@GraphQLArgument(name = CrudApi.id) String id) {
     return super.deleteOne(id);
   }
-  
-  @GraphQLSubscription
-  public Publisher<MessageEntity> incomingMessages(String token) {
-    var user = authService.getUserFromToken(token);
-    if (user.isEmpty()) {
-      throw new InvalidTokenException("Invalid token, either user doesn't exist or token invalid", token);
-    }
-    
-    //TODO:
-    return null;
-  }
+
 }
 
 
