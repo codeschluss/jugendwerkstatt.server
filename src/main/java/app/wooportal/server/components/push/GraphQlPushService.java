9fba8e3135cb9f29ddc9e89397d7bc23d371f940
vetemi <valmir@codeschluss.de>
2022-06-10 10:51:58 +0200

    little cleanup

--- src/main/java/app/wooportal/server/components/push/GraphQlPushService.java
+++ src/main/java/app/wooportal/server/components/push/GraphQlPushService.java
@@ -1,6 +1,5 @@
 package app.wooportal.server.components.push;
 
-import java.io.IOException;
 import java.util.Optional;
 import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Service;
@@ -9,20 +8,12 @@ import io.leangen.graphql.spqr.spring.util.ConcurrentMultiMap;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
-
 @Service
 public class GraphQlPushService {
 
-
-  private final ConcurrentMultiMap<String, FluxSink<MessageDto>> subscribers =
-      new ConcurrentMultiMap<>();
-
-  public GraphQlPushService(PushConfig config)
-      throws IOException {
-  }
+  private final ConcurrentMultiMap<String, FluxSink<MessageDto>> subscribers = new ConcurrentMultiMap<>();
 
   public Publisher<MessageDto> addListener(Optional<UserEntity> user) {
-
     return Flux.create(
         subscriber -> subscribers.add(user.get().getId(),
             subscriber.onDispose(() -> subscribers.remove(user.get().getId(), subscriber))),
@@ -32,6 +23,5 @@ public class GraphQlPushService {
   public void sendPush(UserEntity user, MessageDto message) {
       subscribers.get(user.getId())
         .forEach(subscriber -> subscriber.next(message));
-      
   }
 }
