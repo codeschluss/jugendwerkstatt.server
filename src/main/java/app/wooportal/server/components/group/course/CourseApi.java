5953dc46bcd5f5e68a797d5b53210df6eb37b1b6
vetemi <valmir@codeschluss.de>
2022-06-13 14:25:11 +0200

    preparation for #241

--- src/main/java/app/wooportal/server/components/group/course/CourseApi.java
+++ src/main/java/app/wooportal/server/components/group/course/CourseApi.java
@@ -2,6 +2,7 @@ package app.wooportal.server.components.group.course;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import org.springframework.stereotype.Component;
 import app.wooportal.server.core.base.CrudApi;
 import app.wooportal.server.core.base.dto.listing.FilterSortPaginate;
@@ -9,6 +10,7 @@ import app.wooportal.server.core.base.dto.listing.PageableList;
 import app.wooportal.server.core.security.permissions.AdminPermission;
 import app.wooportal.server.core.security.permissions.ApprovedAndVerifiedPermission;
 import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLContext;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
@@ -63,6 +65,14 @@ public class CourseApi extends CrudApi<CourseEntity, CourseService> {
   @AdminPermission
   public Boolean deleteOne(@GraphQLArgument(name = CrudApi.id) String id) {
     return super.deleteOne(id);
+  }
+  
+  @GraphQLQuery(name = "averageRating")
+  @AdminPermission
+  public CompletableFuture<Double> calculateAverageRating(@GraphQLContext CourseEntity course) {
+    return CompletableFuture.supplyAsync(() -> {
+      return 0.0;
+    });
   }
 
 }
