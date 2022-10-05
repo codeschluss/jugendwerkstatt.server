0892e57b27819cd3e7c7fc3ee4bd2f76131df323
vetemi <valmir@codeschluss.de>
2022-10-05 13:43:44 +0200

    opened API for search

--- src/main/java/app/wooportal/server/components/search/SearchApi.java
+++ src/main/java/app/wooportal/server/components/search/SearchApi.java
@@ -3,7 +3,6 @@ package app.wooportal.server.components.search;
 import java.util.List;
 import org.springframework.stereotype.Component;
 import app.wooportal.server.core.base.dto.listing.FilterSortPaginate;
-import app.wooportal.server.core.security.permissions.ApprovedAndVerifiedPermission;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
@@ -14,12 +13,10 @@ public class SearchApi {
   private final SearchService searchService;
 
   public SearchApi(SearchService searchService) {
-
     this.searchService = searchService;
   }
 
   @GraphQLQuery(name = "search")
-  @ApprovedAndVerifiedPermission
   public List<SearchDto> search(FilterSortPaginate params) {
     return searchService.search(params);
   }
